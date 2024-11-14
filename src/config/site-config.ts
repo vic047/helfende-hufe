bd854b8c416757932a07bfa957e17e0be7ab8afb
Vico Shortman <vico@shortman.me>
2024-11-14 13:07:56 +0100

    fixed typo in siteConfig

--- src/config/site-config.ts
+++ src/config/site-config.ts
@@ -1,5 +1,5 @@
 export const siteConfig: SiteConfig = {
-  siteName: "Helfende Hude e.V.",
+  siteName: "Helfende Hufe e.V.",
   logoUrl:
     "https://image.jimcdn.com/app/cms/image/transf/dimension=180x10000:format=jpg/path/sf18e5aed4508be0a/image/ic6f0afd732d8d1f2/version/1480884411/image.jpg",
   navigator: {
